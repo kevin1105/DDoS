532f7f064b46ed2aba77610ef938ac1dbefd45d9
kevin1105 <31319917+kevin1105@users.noreply.github.com>
2018-05-08 23:03:11 -0700

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -69,10 +69,10 @@ def dos2():
 
 
 def usage():
-	print (''' \033[92m	Hammer Dos Script v.1 http://www.canyalcin.com/
+	print (''' \033[92m	
 	It is the end user's responsibility to obey all applicable laws.
 	It is just for server testing script. Your ip is visible. \n
-	usage : python3 hammer.py [-s] [-p] [-t]
+	usage : python3 ddos.py [-s] [-p] [-t]
 	-h : help
 	-s : server ip
 	-p : port default 80
